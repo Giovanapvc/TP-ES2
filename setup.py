48ecfa33098dd5e3713301b03326635a4fbb18a3
Giovanapvc <giovanapiorino92@gmail.com>
2025-06-13 20:31:13 -0300

    update for setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="tpes2",
+    version="0.1",
+    py_modules=["cli", "entities", "export_html", "repo", "service"],
+    install_requires=[
+        "click",      # se usar click
+    ],
+)
